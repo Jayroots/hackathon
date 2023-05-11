3b6bd9b12b0785548a76565d298cde7ad79f7504
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-05-11 22:08:24 +0200

    grand ma et logo terre anime

--- src/pages_components/HomePage.jsx
+++ src/pages_components/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import imageterre from "./../assets/images/la-terre.png";
 import imagegrandmere from "./../assets/images/GrandMere.png";
+
 import Carrousel from '../components/Carrousel';
 const HomePage = () => {
+
     return (
       <div>
          <div className='homePage'>
-           <h1 className='titrePage'>Look Grand’MA</h1>
+         
+           <h1 className='titrePage'><img className='imageterre' src={imageterre}/>Look Grand’MA</h1>
              <img className='imagegrandmere' src={imagegrandmere}/>
              <Carrousel />
                </div>
