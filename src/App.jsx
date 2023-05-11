8200d72fffd2708b1d992f7b23618467294a1349
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-05-11 15:08:02 +0200

    router ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react'
-
+import { Routes, Route } from "react-router-dom";
 import './import.scss'
+import HomePage from "./pages_components/HomePage"
+import Contact from "./pages_components/Contact"
 
 function App() {
-
-
   return (
     <>
-
+      <main className="appStyle">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
