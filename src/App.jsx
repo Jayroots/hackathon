60b2b62d7dd80937c5bc86c6da4daf65f7928d9d
JEREMY FRANCOIS <JEREM.FRANCOIS@gmail.com>
2023-05-11 15:16:47 +0200

    envoie navbar et footer a styliser

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
-
+import NavBar from './components/global/NavBar'
 import './import.scss'
+import Footer from './components/global/Footer'
+
 
 function App() {
 
 
   return (
     <>
-
+      <NavBar />
+      <Footer />
     </>
   )
 }
