d094a13d99c94b4f8c18d154e06aef639b76c3f8
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-05-11 21:06:14 +0200

    mamie et carouselle amis

--- src/pages_components/HomePage.jsx
+++ src/pages_components/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import imagegrandmere from "./../assets/images/GrandMere.png";
-
+import Carrousel from '../components/Carrousel';
 const HomePage = () => {
     return (
-        <div className='homePage'>
-            <h1 className='titrePage'>Look Grand’MA</h1>
-           <img className='imagegrandmere' src={imagegrandmere}/>
+      <div>
+         <div className='homePage'>
+           <h1 className='titrePage'>Look Grand’MA</h1>
+             <img className='imagegrandmere' src={imagegrandmere}/>
+             <Carrousel />
+               </div>
+            
            </div>
-        
     );
 };
 
