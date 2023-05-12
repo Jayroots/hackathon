a02daaeea8a42f56b6a312dd9bb63e673c92a332
JEREMY FRANCOIS <JEREM.FRANCOIS@gmail.com>
2023-05-12 12:10:47 +0200

    reparation site manque insertion api + css juju

---
+++ src/components/AlbumCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AlbumCards = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AlbumCards;
--- src/pages_components/CreateAlbum.jsx
+++ src/pages_components/CreateAlbum.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import AlbumCards from '../components/global/AlbumCards';
+/* import AlbumCards from '../components/global/AlbumCards'; */
 import imagealbum from "./../assets/images/album-photo.png";
 
 const CreateAlbum = () => {
     return (
         <div>
-             <h1 className='titrePage'><img className='imageterre' src={imagealbum}/>Look Grand’MA</h1>
-           <h2>Pas le temps d'appeler Mamie ?<br></br>
-            Et pourtant tu es sur ton ordi ? 
-           </h2>
+            <h1 className='titrePage'><img className='imageterre' src={imagealbum} />Look Grand’MA</h1>
+            <h2>Pas le temps d'appeler Mamie ?<br></br>
+                Et pourtant tu es sur ton ordi ?
+            </h2>
         </div>
     );
 };
