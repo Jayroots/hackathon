23bfc2f8e9dc9056dd4fc744793ef875638b5ab3
JEREMY FRANCOIS <JEREM.FRANCOIS@gmail.com>
2023-05-12 12:20:47 +0200

    version avant intergration Cards

--- src/pages_components/CreateAlbum.jsx
+++ src/pages_components/CreateAlbum.jsx
@@ -5,10 +5,7 @@ import imagealbum from "./../assets/images/album-photo.png";
 const CreateAlbum = () => {
     return (
         <div>
-            <h1 className='titrePage'><img className='imageterre' src={imagealbum} />Look Grand’MA</h1>
-            <h2>Pas le temps d'appeler Mamie ?<br></br>
-                Et pourtant tu es sur ton ordi ?
-            </h2>
+
         </div>
     );
 };
--- src/pages_components/HomePage.jsx
+++ src/pages_components/HomePage.jsx
@@ -6,20 +6,20 @@ import Carrousel from '../components/Carrousel';
 
 const HomePage = () => {
 
-    return (
-      <div>
-         <div className='homePage'>
-         <h2>Pas le temps d'appeler Mamie ?<br></br>
-         Et pourtant tu es sur ton ordi ? 
+  return (
+    <div>
+      <div className='homePage'>
+        <h2>Parce que vos souvenirs<br></br>
+          s'immortalise ❤️
         </h2>
-         
-          
-             <ButtonAlbum /><img className='imagegrandmere' src={imagegrandmere}/>
-             <Carrousel />
-               </div>
-            
-           </div>
-    );
+
+
+        <ButtonAlbum /><img className='imagegrandmere' src={imagegrandmere} />
+        <Carrousel />
+      </div>
+
+    </div>
+  );
 };
 
 export default HomePage;
