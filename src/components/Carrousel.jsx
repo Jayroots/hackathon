06eca178e96e04c875ca37380d97394a011f28bd
kaezersoze <fingerzjbm@gmail.com>
2023-05-12 11:09:46 +0200

    CJcarousel85%

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,4 +1,12 @@
 import  { useState } from 'react';
+import cj1 from '../assets/images/cj1.jpg'
+import cj2 from '../assets/images/cj2.jpg'
+import cj3 from '../assets/images/cj3.jpg'
+import cj4 from '../assets/images/cj4.jpg'
+import cj5 from '../assets/images/cj5.jpg'
+import cj6 from '../assets/images/cj6.jpg'
+import cj7 from '../assets/images/cj7.jpg'
+import cj8 from '../assets/images/cj8.jpg'
 
 const Carrousel = () => {
   const [angle, setAngle] = useState(0);
@@ -20,14 +28,14 @@ const Carrousel = () => {
      
       <div id="carousel">
         <figure id="spinner">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/still-lake.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/paradise.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/wanaka-tree.jpg" alt="" />
+          <img src={cj1}alt="" />
+          <img src={cj2}alt="" />
+          <img src={cj3} alt="" />
+          <img src={cj4} alt="" />
+          <img src={cj5}alt="" />
+          <img src={cj6} alt="" />
+          <img src={cj7} alt="" />
+          <img src={cj8} alt="" />
         </figure>
       </div>
       <span
