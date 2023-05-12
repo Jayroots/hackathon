f6b9b008e41f68906e0223f93284e2f2875a868f
kaezersoze <fingerzjbm@gmail.com>
2023-05-12 10:00:55 +0200

    CJbranchcarousel60%

--- src/pages_components/HomePage.jsx
+++ src/pages_components/HomePage.jsx
@@ -3,11 +3,13 @@ import imageterre from "./../assets/images/la-terre.png";
 import imagegrandmere from "./../assets/images/GrandMere.png";
 
 import Carrousel from '../components/Carrousel';
+
 const HomePage = () => {
 
     return (
       <div>
          <div className='homePage'>
+         
          
            <h1 className='titrePage'><img className='imageterre' src={imageterre}/>Look Grand’MA</h1>
              <img className='imagegrandmere' src={imagegrandmere}/>
